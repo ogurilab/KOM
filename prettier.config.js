8747f8e9b022ba4bafad127c88f8d1754ec4cc24
shouta0715 <Myheroes0715@gmail.com>
2023-09-02 14:47:11 +0900

    setting prettier eslint

---
+++ prettier.config.js
@@ -0,0 +1,6 @@
+// prettier.config.js
+module.exports = {
+  tailwindConfig: "./tailwind.config.ts",
+  tailwindFunctions: ["clsx"],
+  formatOnSave: true,
+};
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,44 @@
-import type { Config } from 'tailwindcss'
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable global-require */
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+        inter: ["Inter var", "sans-serif"],
+        sans: ["Inter var", "sans-serif"],
+      },
+      animation: {
+        spinner: "spinner 2s linear infinite",
+        "spinner-child": "spinner-child 1.5s linear infinite",
+      },
+      keyframes: {
+        spinner: {
+          "100%": { transform: "rotate(360deg)" },
+        },
+        "spinner-child": {
+          "0%": {
+            "stroke-dasharray": "0 150",
+            "stroke-dashoffset": "0",
+          },
+          "47.5%": { "stroke-dasharray": "42 150", "stroke-dashoffset": "-16" },
+          "95%,100%": {
+            "stroke-dasharray": "42 150",
+            "stroke-dashoffset": "-59",
+          },
+        },
       },
     },
   },
-  plugins: [],
-}
-export default config
+  plugins: [
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/aspect-ratio"),
+  ],
+};
+export default config;
