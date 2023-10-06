05cffbd8a68e3a6312269a278069dc7d0c8ecf55
shouta0715 <Myheroes0715@gmail.com>
2023-10-06 20:35:49 +0900

    fix fieldset

--- src/features/auth/components/index.tsx
+++ src/features/auth/components/index.tsx
@@ -60,7 +60,7 @@ function AuthForm() {
 
   return (
     <form onSubmit={onSubmitHandler} className="space-y-6">
-      <div>
+      <fieldset>
         <label
           htmlFor="email"
           className="block text-sm font-medium leading-6 text-gray-900"
@@ -79,7 +79,7 @@ function AuthForm() {
             className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6"
           />
         </div>
-      </div>
+      </fieldset>
 
       <div>
         <label
